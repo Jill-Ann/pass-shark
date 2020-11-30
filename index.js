00a2071a90a1c332579c7151d79881afe24c3367
Jill-Ann <jillannkay@gmail.com>
2020-11-30 22:47:20 +0100

    Creat index.js and add function

---
+++ index.js
@@ -0,0 +1,13 @@
+const validate = (username, password) => {
+  const substrLength = Math.ceil(
+    Math.min(username.length, password.length) / 2
+  );
+
+  for (i = 0; i <= username.length - substrLength; i++) {
+    let substr = username.substring(i, i + substrLength);
+    if (password.includes(substr)) return false;
+  }
+  return true;
+};
+
+module.exports = validate;
