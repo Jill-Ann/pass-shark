100524ae24bbb37e486dc4a75fa654e8d7aa26e9
Jill-Ann <jillannkay@gmail.com>
2020-12-01 17:24:37 +0100

    Change substring to slice

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const validate = (username, password) => {
   if (typeof username !== "string" || typeof password !== "string") {
     throw new TypeError("Please pass in a string");
   }
+
   const substrLength = Math.ceil(
     Math.min(username.length, password.length) / 2
   );
 
   for (i = 0; i <= username.length - substrLength; i++) {
-    let substr = username.substring(i, i + substrLength);
+    let substr = username.slice(i, i + substrLength);
+
     if (password.includes(substr)) return false;
   }
+
   return true;
 };
 
